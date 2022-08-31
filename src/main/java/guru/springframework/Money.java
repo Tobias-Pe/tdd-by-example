b94d2d89e12eb5013bf80e83dbc8db6228da45b9
tobias.peslalz <tobias.peslalz@muenchen.de>
2022-08-31 14:12:55 +0200

    :sparkles: makin' objects chapter

---
+++ src/main/java/guru/springframework/Money.java
@@ -0,0 +1,30 @@
+package guru.springframework;
+
+import java.util.Objects;
+
+public abstract class Money {
+    protected int amount;
+
+    public static Money dollar(int amount) {
+        return new Dollar(amount);
+    }
+
+    public abstract Money times(int multiplier);
+
+    public static Money franc(int amount) {
+        return new Franc(amount);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Money)) return false;
+        Money money = (Money) o;
+        return amount == money.amount && this.getClass().equals(o.getClass());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(amount);
+    }
+}
---
+++ src/test/java/guru/springframework/MoneyTest.java
@@ -0,0 +1,35 @@
+package guru.springframework;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
+
+class MoneyTest {
+    @Test
+    void testMultiplicationDollar() {
+        Money five = Money.dollar(5);
+        assertEquals(Money.dollar(10), five.times(2));
+        assertEquals(Money.dollar(15), five.times(3));
+    }
+
+    @Test
+    void testEqualityDollar() {
+        assertEquals(Money.dollar(5), Money.dollar(5));
+        assertNotEquals(Money.franc(5), Money.dollar(5));
+        assertNotEquals(Money.dollar(6), Money.dollar(5));
+    }
+
+    @Test
+    void testMultiplicationFranc() {
+        Money five = Money.franc(5);
+        assertEquals(Money.franc(10), five.times(2));
+        assertEquals(Money.franc(15), five.times(3));
+    }
+
+    @Test
+    void testEqualityFranc() {
+        assertEquals(Money.franc(5), Money.franc(5));
+        assertNotEquals(Money.franc(6), Money.franc(5));
+    }
+}
